a345a3264901cacbd4975d91d18148951350aa69
Nikhhil Gupta <nikhil106gupta@gmail.com>
2026-04-01 13:25:32 +0530

    introduction to react

---
+++ introduction/i.jsx
@@ -0,0 +1,11 @@
+function counter() {
+    const [count, setCount] = React.useState(0);
+    return (
+        <div>
+            <p>You clicked {count} times</p>
+            <button onClick={() => setCount(count + 1)}>
+                Click me
+            </button>
+        </div>
+    );
+}
