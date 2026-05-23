e04ae3c8fc16376536d601260550b33471bdcc9c
Nikhhil Gupta <nikhil106gupta@gmail.com>
2026-05-23 20:16:41 +0530

    exp-6a

---
+++ Experiment-6a/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import Navbar from './component/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Notfound from './pages/Notfound';
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+       {/* 404 Not Found Route */}
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </Router>
+    </>
+  )
+}
+
+export default App
